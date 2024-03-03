23297fc8eda7944203223e4ccf1988e14fcf9d6a
Matheus Kamer <matheus_kamer@hotmail.com>
2024-03-03 17:45:51 -0400

    feat: create server on port 3333

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application } from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+class App{
+  private app: Application;
+  private http: http.Server;
+  private io: Server;
+  constructor() {
+    this.app = express();
+    this.http = new http.Server(this.app);
+    this.io = new Server(this.http)
+  }
+
+  public listen(){
+    this.app.listen(3333, () => {
+      console.log('Server is running on port 3333🔥!');
+    })
+  }
+}
+
+export { App };
